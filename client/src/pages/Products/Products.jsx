4e9089de02a6acc75d8f72d3c83d6aadce617ebf
Jadyn <jadyngg19@gmail.com>
2023-07-05 21:07:01 -0400

    Style Products

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,85 +1,104 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
-import "./Products.scss"; 
+import "./Products.scss";
 import List from '../../components/List/List';
 import useFetch from '../../hooks/useFetch';
+import { FiArrowRight } from 'react-icons/fi';
 
-export const Products = () => { 
+export const Products = () => {
+  const catId = parseInt(useParams().id);
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState();
+  const [selectedSubCats, setSelectedSubCats] = useState([]);
+  const [sidebarVisible, setSidebarVisible] = useState(true); // Track sidebar visibility
 
-  const catId = parseInt(useParams().id); 
-  const [maxPrice, setMaxPrice] = useState(1000); 
-  const [sort, setSort] = useState(); 
-  const [selectedSubCats, setSelectedSubCats] = useState([]); 
-
-  const {data, loading, error} = useFetch(
+  const { data, loading, error } = useFetch(
     `/sub-categories?[filters][categories][id][$eq]=${catId}`
-    ); 
+  );
 
   const handleChange = (e) => {
-    const value = e.target.value; 
-    const isChecked = e.target.checked; 
+    const value = e.target.value;
+    const isChecked = e.target.checked;
 
     setSelectedSubCats(
-      isChecked 
-        ? [...selectedSubCats, value] 
+      isChecked
+        ? [...selectedSubCats, value]
         : selectedSubCats.filter((item) => item !== value)
-      ); 
-  }; 
+    );
+  };
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
 
   return (
     <div className="products">
-      <div className="left">
+      <div className={`left ${sidebarVisible ? '' : 'minimized'}`}>
         <br />
+        <br /> 
+        <br /> 
         <div className="filterItem">
           <h2>Product Categories</h2>
           {data?.map((item) => (
             <div className="inputItem" key={item.id}>
-              <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} /> 
+              <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} />
               <label htmlFor={item.id}>{item.attributes.title}</label>
             </div>
           ))}
         </div>
         <div className="filterItem">
-          <h2>Filter by price</h2> 
+          <h2>Filter by price</h2>
           <div className="inputItem">
-            <span>0</span> 
-              <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)}/> 
+            <span>0</span>
+            <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
             <span>{maxPrice}</span>
           </div>
         </div>
         <div className="filterItem">
-          <h2>Sort by</h2> 
+          <h2>Sort by</h2>
           <div className="inputItem">
-            <input 
-              type="radio" 
-              id=":asc" 
-              value=":asc" 
-              name="price" 
-              onChange={(e) => setSort(":asc")} 
-            /> 
+            <input
+              type="radio"
+              id=":asc"
+              value=":asc"
+              name="price"
+              onChange={(e) => setSort(":asc")}
+            />
             <label htmlFor=":asc">Price (Lowest first)</label>
           </div>
           <div className="inputItem">
-            <input 
-              type="radio" 
-              id=":desc" 
-              value=":desc" 
-              name="price" 
-              onChange={(e) => setSort(":desc")} 
-            /> 
+            <input
+              type="radio"
+              id=":desc"
+              value=":desc"
+              name="price"
+              onChange={(e) => setSort(":desc")}
+            />
             <label htmlFor=":desc">Price (Highest first)</label>
           </div>
         </div>
       </div>
       <div className="right">
+        <div className={`toggleSidebar ${sidebarVisible ? '' : 'minimized'}`}>
+          <FiArrowRight className="toggleIcon" onClick={toggleSidebar} />
+        </div>
         <br />
         <br />
-        <img 
-          className="catImg" 
-          src={catId === 1 ? "/img/totes.jpg" : catId === 2 ? "/img/buckets.jpg" : catId === 3 ? "/img/crosses.jpg" : "/img/clutches.jpg"} 
+        <br /> 
+        <img
+          className="catImg"
+          src={
+            catId === 1
+              ? "/img/totes.jpg"
+              : catId === 2
+              ? "/img/buckets.jpg"
+              : catId === 3
+              ? "/img/crosses.jpg"
+              : "/img/clutches.jpg"
+          }
           alt=""
-          /> 
-          <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} /> 
+        />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
       </div>
     </div>
   );
