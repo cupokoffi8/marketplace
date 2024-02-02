91726043db5529f474c134e3e95d7c271fe42771
Jadyn <jadyngg19@gmail.com>
2024-02-01 19:21:22 -0500

    Update axios baseUrl -> http, !https

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios"; 
 
 export const makeRequest = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, 
+    baseURL: `http://${process.env.REACT_APP_API_URL}`, 
     headers: {
         Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
-    }
-})
+    },
+    withCredentials: true,
+});
